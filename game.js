b12d5aa25506c14dbb914054b20ed41aa826d829
Alien-AV <alienav@gmail.com>
2025-07-18 22:18:38 -0400

    Display heat progress as a bar

--- game.js
+++ game.js
@@ -112,7 +112,8 @@ class Game {
     document.getElementById('patrol').textContent = s.patrol;
     document.getElementById('territory').textContent = s.territory;
     document.getElementById('heat').textContent = s.heat;
-    document.getElementById('heatProgress').textContent = s.heatProgress;
+    const heatBar = document.getElementById('heatProgressBar');
+    heatBar.style.width = (s.heatProgress * 10) + '%';
     document.getElementById('disagreeableOwners').textContent = s.disagreeableOwners;
     document.getElementById('fear').textContent = s.fear;
     document.getElementById('businesses').textContent = s.businesses;
